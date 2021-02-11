22197ca4f5d9c660720dec215bd00e779d0be1c9
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2021-02-11 11:33:49 -0600

    implemented method to remove duplicates

--- src/TokenizeSortRemoveAddApp.java
+++ src/TokenizeSortRemoveAddApp.java
@@ -19,8 +19,29 @@ public class TokenizeSortRemoveAddApp {
 		ArrayList<String > sortedTokenizedSentence = new ArrayList<String>();
 		sortedTokenizedSentence = sortTheArrayList(tokenizedSentence); //task three completed for 4 points
 		
+		printArrayList(sortedTokenizedSentence);
 		
+		ArrayList<String> listNoDuplicates = new ArrayList<String>();
+		
+		listNoDuplicates = removeDuplicates(sortedTokenizedSentence);
+		
+		printArrayList(listNoDuplicates);
 
+	}
+
+	private static ArrayList<String> removeDuplicates(ArrayList<String> list) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0;i<list.size();i++) {
+			for(int j=i+1; j<list.size(); j++) {
+				if(list.get(i).equals(list.get(j))){
+					list.remove(j);
+					j--;
+				}
+			}
+		}
+		
+		return list;
 	}
 
 	private static ArrayList<String> sortTheArrayList(ArrayList<String> tokenizedSentence) {
